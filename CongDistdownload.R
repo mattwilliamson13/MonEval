e1787b9c7e7f051269682cf733e5a4e2cdbc5840
mattwilliamson13 <mwilliamson@ucdavis.edu>
2017-05-30 13:54:37 -0700

    script to download Congressional District zipfiles

---
+++ CongDistdownload.R
@@ -0,0 +1,15 @@
+setwd("C:/ConsOppData/SpatialData/CongressionalDistricts/")
+rt <- "http://cdmaps.polisci.ucla.edu/shp/districts"
+cn <- seq(from=92, to=114, by = 1)
+cnp <- ifelse(cn < 100, paste0("0",cn), cn)
+ext <- ".zip"
+
+for(i in 1:length(cnp)){
+  y <- cnp[i]
+  link <- paste0(rt,y,ext)
+  fname <- paste0("districts",y,ext)
+  download.file(url=link, destfile=fname)
+}
+
+dist.zip <- list.files(getwd(), pattern=".zip")
+lapply(dist.zip,unzip(x) junkpaths=TRUE)
