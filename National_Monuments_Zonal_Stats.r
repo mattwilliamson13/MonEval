65a46d02ff085e59c9ece21fe3c2c4b82ad3ce28
Tyler <tyler@largelandscapes.org>
2017-05-31 16:56:26 -0600

    Some updates to ecol systems diversity code - still in progress.

--- National_Monuments_Zonal_Stats.r
+++ National_Monuments_Zonal_Stats.r
@@ -32,6 +32,18 @@ for(i in 1:length(inputnames)) {  # calculate zonal stats for each input raster
 ecol.systems <- raster::extract(natlandcover, PA.sp, na.rm=TRUE)  # list of ecological systems (by ID) within each PA
 system.diversity <- lapply(ecol.systems, function(x) length(unique(x)))  # number of different systems within each PA
 # THIS IS TOO SIMPLE - DOES NOT ACCOUNT FOR FACT THAT PA SIZE IS HIGHLY VARIABLE, AND ECOL SYSTEM DIVERSITY DEPENDS ON SIZE
+  # new steps needed:
+  # 1. Extract all lands within each Bailey's province
+  # 2. Calculate area of each ecological system within the province (these will be rarity weights)
+  # 3. For each PA, calculate number of ecological systems within the PA
+  # 4. Calculate rarity-weighted richness of ecological systems: summation of 1/Ci (where Ci is the area of system i within the province)
+  # 5. Standardize rarity-weighted richness by area of PA (to account for fact that larger areas should have greater representation of systems by virtue of size alone)
+  # 6. Repeat calculation but use all fed lands within the province, rather than a specific PA
+bailey.sp <- as(bailey, "Spatial") # convert sf polygon layer to a spatial layer first (required for extract function)
+tempA <- raster::extract(natlandcover, bailey.sp, na.rm=TRUE) # list of ecological systems within each Bailey's province
+tempB <- lapply(tempA, function(x), length(x))  # area of province (in # of cells from natlandcover)
+tempC <- lapply(tempA, function(x), table(x))  # get number of cells of each ecological system within the province
+tempD <- raster::extract(natlandcover, PA.sp, na.rm=TRUE)
 
 ### PA ZONAL STATISTICS FOR VECTOR INPUTS ###
 
@@ -44,6 +56,7 @@ mean.rich.fish <- st_intersection(rich.fish, PA) %>%  # intersect PAs and richne
   mutate(weightedValue = overlapProportion * Join_Count) %>%  # weighted value = weight x richness value
   group_by(UnitName) %>%
   summarise(weightedMean = sum(weightedValue))
+
 mean.rich.amphib <- st_intersection(rich.amphib, sample20) %>%  # intersect PAs and richness polygons
   mutate(intersectPolyArea =  as.numeric(st_area(geometry))) %>%  # calculate areas of intersection polygons
   group_by(UnitName) %>%  # group intersection polygons by PA
