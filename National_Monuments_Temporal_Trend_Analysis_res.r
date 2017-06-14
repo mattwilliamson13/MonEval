142107cf9398babbc4211600a5eee449b5650b4e
Tyler <tyler@largelandscapes.org>
2017-06-14 11:44:28 -0600

    Removed duplicate code from temporal trends

--- National_Monuments_Temporal_Trend_Analysis_res.r
+++ National_Monuments_Temporal_Trend_Analysis_res.r
@@ -2,7 +2,7 @@
 ## NM DESIGNATIONS THROUGH TIME
 
 library(dplyr)
-library(tidyr)
+library(tidyr)  
 
 data <- read.csv("C:/Work/SharedDocs/CLLC/NationalMonuments/AA_designations_spreadsheet_formatted.csv", header=TRUE, stringsAsFactors=FALSE)  # load database
 tbl_df(data)  # convert to tbl class
@@ -10,26 +10,5 @@ View(data)
 data %>% group_by(ActionType) %>% summarise(TotalAcres=sum(AcresAffect, na.rm=TRUE))
 
 
-
-
-
 sort(unique(data$Action))
 hist(data$Year[which(data$Action=="Established")])
-
-## NM DESIGNATIONS THROUGH TIME
-
-library(dplyr)
-library(tidyr)
-
-data <- read.csv("C:/Work/SharedDocs/CLLC/NationalMonuments/AA_designations_spreadsheet_formatted.csv", header=TRUE, stringsAsFactors=FALSE)  # load database
-tbl_df(data)  # convert to tbl class
-View(data)
-data %>% group_by(ActionType) %>% summarise(TotalAcres=sum(AcresAffect, na.rm=TRUE))
-
-
-
-
-
-sort(unique(data$Action))
-hist(data$Year[which(data$Action=="Established")])
-
