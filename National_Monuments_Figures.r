1b29fa18f8c8361f3399801cf3569f63033d05a2
Tyler <tyler@largelandscapes.org>
2017-06-16 16:44:29 -0600

    Create new script for generating ggplot2 figures

---
+++ National_Monuments_Figures.r
@@ -0,0 +1,73 @@
+library(raster)
+library(sp)
+library(sf)
+library(rgeos)
+library(rgdal)
+library(dplyr)
+library(ggplot2)
+library(viridis)
+library(tidyverse)
+library(rvest)
+library(maptools)
+library(ggplot2)
+
+infolder <- "C:/Users/Tyler/Google Drive/MonumentData/Generated Data"  # set folder holding input data
+load(paste(infolder, "/WSnofedland.RData", sep="")) # load workspace with prepped input layers
+
+
+
+####################################################################################
+### DATA EXPLORATION
+
+# Histogram of # PAs designated per year by Congress vs. President
+ggplot() +
+  geom_histogram(data=PA, aes(x = EstabYear, fill=DesigAuth), binwidth=1)
+# Frequency plot (like histogram, but with lines instead of bars)
+ggplot() +
+  geom_freqpoly(data=PA, aes(x = EstabYear, color=DesigAuth), binwidth=1)
+# Histogram with faceting
+ggplot() +
+  geom_histogram(data=PA, aes(x = EstabYear, fill=DesigAuth), binwidth=1) +
+  facet_wrap(~DesigAuth, ncol=1)
+
+# boxplots of acreage of CPAs versus PNMs
+ggplot() +
+  geom_boxplot(data=PA, aes(x=DesigAuth, y=area_ac), fill=c("red","green","blue"), width=0.2) +
+  scale_x_discrete(name="Designating authority") +
+  scale_y_continuous(name="Protected area acreage") +
+  theme_bw()
+
+# violin plot (similar to boxplot)
+ggplot() +
+  geom_violin(data=PA, aes(x=DesigAuth, y=area_ac)) +
+  scale_x_discrete(name="Designating authority") +
+  scale_y_continuous(name="Protected area acreage") +
+  theme_bw()
+
+# scatterplots of establishment year vs PA area, with faceting by designating authority
+ggplot(data=PA, aes(x=EstabYear, y=area_ac)) +
+  geom_point() +
+  facet_wrap(~DesigAuth)
+
+
+
+#################################################################################################
+### FIGURE 1: MAP OF PROTECTED AREAS COLOR CODED BY DESIGNATION TYPE
+
+PA <- as(PA, "sf")  # make sure PA layer is in sf format
+
+gg1 <- ggplot() +
+  geom_sf(lower48, aes())
+
+ggplot() +
+  geom_sf(data=bailey) +
+  geom_sf(data=PA, aes(fill=DesigAuth)) +
+  ggtitle("Federal protected areas of the contigous United States") +
+  theme_bw()
+
+# would like to add lower48 outline, but can't use geom_sf with sfc class (i.e., multipolygon feature)
+
+
+
+##########################################################################################
+### FIGURE 2: BOXPLOT OF 
