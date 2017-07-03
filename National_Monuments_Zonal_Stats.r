55404888651ffda9540c03c5949ac09fb80ec5ea
mattwilliamson13 <mwilliamson@ucdavis.edu>
2017-07-03 11:04:01 -0700

    modify for schwartzserver and include max calculation

--- National_Monuments_Zonal_Stats.r
+++ National_Monuments_Zonal_Stats.r
@@ -13,8 +13,8 @@ library(rvest)
 library(maptools)
 library(ggplot2)
 
-infolder <- "C:/Users/Tyler/Google Drive/MonumentData/Generated Data"  # location on Tyler's Google Drive
-#infolder <- "D:/Data/MonumentData/Generated Data"  # location on Schwartz server
+#infolder <- "C:/Users/Tyler/Google Drive/MonumentData/Generated Data"  # location on Tyler's Google Drive
+infolder <- "D:/Data/MonumentData/Generated Data"  # location on Schwartz server
 
 rasterOptions(progress="text", overwrite=TRUE)  # turn on progress bar for raster operations
 
@@ -49,7 +49,19 @@ fun = "mean"  # choose function for zonal stats (e.g., mean, min, max, sum)
 inputnames <- c("climate", "rich.bird", "rich.mammal", "rich.tree", "rich.reptile")  # rasters for which we want to calculate zonal stats
 outputnames <- paste(fun,inputnames,sep=".") # vectors that will hold output values
 for(i in 1:length(inputnames)) {  # calculate zonal stats for each input raster
+  start <- Sys.time()
   zonalvals <- raster::extract(x=get(inputnames[i]), y=PA.sp, method=simple, fun=get(fun), na.rm=TRUE, weights=TRUE, normalizeWeights=TRUE, df=FALSE)
+  assign(outputnames[i],zonalvals)
+  end <- Sys.time()
+  process <- end - start
+  print(paste0(i, "Process=", process))
+}
+
+fun = "max"  # choose function for zonal stats (e.g., mean, min, max, sum)
+inputnames <- c("climate", "rich.bird", "rich.mammal", "rich.tree", "rich.reptile")  # rasters for which we want to calculate zonal stats
+outputnames <- paste(fun,inputnames,sep=".") # vectors that will hold output values
+for(i in 1:length(inputnames)) {  # calculate zonal stats for each input raster
+  zonalvals <- raster::extract(x=get(inputnames[i]), y=PA.sp, method=simple, fun=get(fun), na.rm=TRUE, df=FALSE)
   assign(outputnames[i],zonalvals)
 }
 
@@ -57,29 +69,29 @@ for(i in 1:length(inputnames)) {  # calculate zonal stats for each input raster
 ### PA ZONAL STATISTICS FOR VECTOR INPUTS ###
 
 # Mean richness
-mean.rich.fish <- st_intersection(rich.fish, PA) %>%  # intersect PAs and richness polygons
+temp.rich.fish <- st_intersection(rich.fish, PA) %>%  # intersect PAs and richness polygons
   mutate(intersectPolyArea =  as.numeric(st_area(geometry))) %>%  # calculate areas of intersection polygons
   group_by(UnitName) %>%  # group intersection polygons by PA
   mutate(sumIntersectArea = sum(intersectPolyArea)) %>% # get the sum of intersect polygon areas associated with each PA (could be different than PA area because of missing data (e.g., watersheds with no fish richness))
   mutate(overlapProportion = intersectPolyArea/sumIntersectArea) %>% # get the proportion of the summed intersect areas associated with each intersect polygon (these are the "weights")
   mutate(weightedValue = overlapProportion * Join_Count) %>%  # weighted value = weight x richness value
   group_by(UnitName) %>%
-  summarise(weightedMean = sum(weightedValue))
+  summarise(weightedMean = sum(weightedValue), max=max(Join_Count))
+mean.rich.fish <- temp.rich.fish$weightedMean
+max.rich.fish <- temp.rich.fish$max
 
-mean.rich.amphib <- st_intersection(rich.amphib, sample20) %>%  # intersect PAs and richness polygons
+temp.rich.amphib <- st_intersection(rich.amphib, sample20) %>%  # intersect PAs and richness polygons
   mutate(intersectPolyArea =  as.numeric(st_area(geometry))) %>%  # calculate areas of intersection polygons
   group_by(UnitName) %>%  # group intersection polygons by PA
   mutate(sumIntersectArea = sum(intersectPolyArea)) %>% # get the sum of intersect polygon areas associated with each PA (could be different than PA area because of missing data (e.g., watersheds with no fish richness))
   mutate(overlapProportion = intersectPolyArea/sumIntersectArea) %>% # get the proportion of the summed intersect areas associated with each intersect polygon (these are the "weights")
   mutate(weightedValue = overlapProportion * Join_Count) %>%  # weighted value = weight x richness value
   group_by(UnitName) %>%
-  summarise(weightedMean = sum(weightedValue))
+  summarise(weightedMean = sum(weightedValue), max=max(Join_Count))
+mean.rich.amphib <- temp.rich.amphib$weightedMean
+max.rich.amphib <- temp.rich.amphib$max
 
 # need to add code here for LCV and sector dominance
-
-
-
-
 
 
 
@@ -121,8 +133,8 @@ pi <- st_intersection(bailey, PA)  # get intersections between polygons in PA an
 piArea <- pi %>%   # get areas of intersections
   mutate(area = st_area(.) %>% as.numeric())
 totalArea <- piArea %>%  # calculate total areas of each bailey division for each PA
-  as_tibble() %>% 
-  group_by(UnitName, DIVISION) %>% 
+  as_tibble() %>%
+  group_by(UnitName, DIVISION) %>%
   summarize(area = sum(area))
 baileyMajority <- totalArea %>%  # for each PA, keep the row with the division that has the largest area
   as_tibble() %>%
@@ -130,32 +142,22 @@ baileyMajority <- totalArea %>%  # for each PA, keep the row with the division t
   top_n(n=1)
 bailey.majority <- baileyMajority$DIVISION  # vector of majority division to include in PA dataframe
 
-## alternatively, create dataframe with column for each bailey division to indicate if each PA (row) intersects that division
-#bailey.df <- data.frame(matrix(0, nrow=nrow(PA), ncol=nrow(bailey)))
-#names(bailey.df) <- paste("bailey", c(1:nrow(bailey)), sep=".")  # add column names
-## fill in dataframe with intersection data
-#for(i in 1:length(intersections)){
-#  subdata <- intersections[[i]]
-#  bailey.df[i,subdata] <- 1
-#}
-
 
 ### COMBINE OUTPUT VARIABLES IN A SINGLE DATAFRAME
 
 PA.df <- tbl_df(PA)[,-ncol(PA)]  # convert to a tbl object (and strip out geometry field)
-outputvars <- c(outputnames, "mean.rich.amphib", "mean.rich.fish", "system.richness", "system.aw.richness", "system.rw.richness", "system.rw.aw.richness", "bailey.majority")  # vector of names of all output variables
+outputvars <- c(outputnames, "mean.rich.amphib", "max.rich.amphib","mean.rich.fish", "max.rich.fish", "system.richness", "system.aw.richness", "system.rw.richness", "system.rw.aw.richness", "bailey.majority")  # vector of names of all output variables
 for(i in 1:length(outputvars)){  # add each output variables as a new column in dataframe
   PA.df <- data.frame(PA.df, get(outputvars[i]))
 }
 names(PA.df)[(ncol(PA.df)-length(outputvars)+1):ncol(PA.df)] <- outputvars # give names to new output variables in dataframe
+
 # add a new variable for ORIGINAL designating authority (i.e., find current parks and preserves that started out as presidential NMs but were later redesignated by Congress)
 PA.df$OrigDesigAuth <- rep(NA, nrow(PA.df))
 for(i in 1:nrow(PA.df)) {
   if(is.na(PA.df$OrigAntiq[i])==TRUE) {
-    PA.df$OrigDesigAuth[i] <- PA.df$DesigAuth[i] 
+    PA.df$OrigDesigAuth[i] <- PA.df$DesigAuth[i]
   } else {
     PA.df$OrigDesigAuth[i] <- "President"
   }
 }
-
-
