6c25f2baa875ba586958f767aa45d9518ee9d42f
Tyler <tyler@largelandscapes.org>
2017-10-20 14:42:53 -0600

    Add fraction NA fix to social zonal stats

--- National_Monuments_Social_Data_Zones.R
+++ National_Monuments_Social_Data_Zones.R
@@ -15,6 +15,7 @@ library(fasterize)
 
 infolder <- "D:/Data/MonumentData/Generated Data/"  # location on Schwartz server (NEED TO ADD NEW PA LAYER AND NATURESERV RICHNESS RASTER)
 outfolder <- "D:/Data/MonumentData/GeneratedData/"
+
 PA <- st_read(paste(infolder, "PA_revised_9-21-17.shp", sep=""), stringsAsFactors=FALSE)
 ind_will_sf <- st_read(paste(infolder,"ind_will_sf.shp", sep=""), stringsAsFactors=FALSE)
 
@@ -27,29 +28,34 @@ soc.data.df <- soc.data.df[order(soc.data.df$UnitName),]
 
 
 for (i in 1:length(buffer_dist)){
-PA.buf <- st_buffer(PA, dist = buffer_dist[i])
-temp.int.df <- st_intersection(ind_will_sf, PA.buf)  # intersect PAs and richness polygons
+  PA.buf <- st_buffer(PA, dist = buffer_dist[i]) %>%  # buffer by specified distance
+    mutate(buffered_area_m2 = as.numeric(st_area(geometry)))  # calculate area of each buffer polygon
+  temp.int.df <- st_intersection(ind_will_sf, PA.buf)  # intersect PAs and county/congressional district polygons
   for (j in 1: length(val_name)){
     temp.sum.df <- temp.int.df %>% mutate(intersectPolyArea =  as.numeric(st_area(geometry))) %>%  # calculate areas of intersection polygons
-    group_by(UnitName) %>%  # group intersection polygons by PA
-    mutate(sumIntersectArea = sum(intersectPolyArea)) %>% # get the sum of intersect polygon areas associated with each PA (could be different than PA area because of missing data (e.g., watersheds with no fish richness))
-    mutate(overlapProportion = intersectPolyArea/sumIntersectArea) %>% # get the proportion of the summed intersect areas associated with each intersect polygon (these are the "weights")
-    mutate(weightedValue = overlapProportion * get(val_name[j])) %>%  # weighted value = weight x richness value
-    group_by(UnitName) %>%
-    summarise(weightedMean = sum(weightedValue), max=max(get(val_name[j])))
-
-# deal with PAs that overlap blank spots in richness map, and are therefore not represented in results of above richness calculation
-  summary.df <- data.frame(UnitName=temp.sum.df$UnitName, weightedMean=temp.sum.df$weightedMean, max=temp.sum.df$max, stringsAsFactors=FALSE) # create dataframe out of temp.rich.fish
-  summary.PAnames <- summary.df$UnitName  # get list of PA names in the richness output
-  all.PAnames <- PA.buf$UnitName  # get list of all PA names, including those missing from richness output
-  '%notin%' <- function(x,y) !(x %in% y)
-  missing.summary.PAnames <- PA.buf$UnitName[which(PA.buf$UnitName %notin% summary.df$UnitName)]  # find PAs missing from richness output
-  summary.df.corrected <- data.frame(UnitName=c(summary.df$UnitName, missing.summary.PAnames),
-                                  mean.val=c(summary.df$weightedMean, rep(NA, length(missing.summary.PAnames))),
-                                  max.val=c(summary.df$max, rep(NA, length(missing.summary.PAnames))))  # add missing PAs to new dataframe with NA for mean and max richness value
-colnames(summary.df.corrected) <- paste(var[j], colnames(summary.df.corrected),buffer_dist[i], sep =".")
-soc.data.df <- cbind(soc.data.df, summary.df.corrected[,2:3])
+      mutate(unweightedValue = get(val_name[j])) %>%
+      filter(is.na(unweightedValue)==FALSE) %>%
+      group_by(UnitName) %>%  # group intersection polygons by PA
+      mutate(sumIntersectArea = sum(intersectPolyArea)) %>% # get the sum of intersect polygon areas associated with each PA (could be different than PA area because of missing data (e.g., watersheds with no fish richness))
+      mutate(overlapProportion = intersectPolyArea/sumIntersectArea) %>% # get the proportion of the summed intersect areas associated with each intersect polygon (these are the "weights")
+      mutate(weightedValue = overlapProportion * get(val_name[j])) %>%  # weighted value = weight x richness value
+      group_by(UnitName) %>%
+      summarise(weightedMean = sum(weightedValue), max=max(get(val_name[j])), prop.nonNA=mean(sumIntersectArea)/mean(buffered_area_m2))
+    # for those PAs with less than 90% coverage of non-NA richness data, assign overall NA value
+    temp.sum.df$weightedMean[temp.sum.df$prop.nonNA<0.9] <- NA
+    temp.sum.df$max[temp.sum.df$prop.nonNA<0.9] <- NA
+    # deal with PAs that overlap blank spots in richness map, and are therefore not represented in results of above richness calculation
+    summary.df <- data.frame(UnitName=temp.sum.df$UnitName, weightedMean=temp.sum.df$weightedMean, max=temp.sum.df$max, stringsAsFactors=FALSE) # create dataframe out of temp.rich.fish
+    summary.PAnames <- summary.df$UnitName  # get list of PA names in the richness output
+    all.PAnames <- PA.buf$UnitName  # get list of all PA names, including those missing from richness output
+    '%notin%' <- function(x,y) !(x %in% y)
+    missing.summary.PAnames <- PA.buf$UnitName[which(PA.buf$UnitName %notin% summary.df$UnitName)]  # find PAs missing from richness output
+    summary.df.corrected <- data.frame(UnitName=c(summary.df$UnitName, missing.summary.PAnames),
+                                       mean.val=c(summary.df$weightedMean, rep(NA, length(missing.summary.PAnames))),
+                                       max.val=c(summary.df$max, rep(NA, length(missing.summary.PAnames))))  # add missing PAs to new dataframe with NA for mean and max richness value
+    colnames(summary.df.corrected) <- paste(var[j], colnames(summary.df.corrected),buffer_dist[i], sep =".")
+    soc.data.df <- cbind(soc.data.df, summary.df.corrected[,2:3])
+  }
 }
-}
 
-write.csv(paste0(outfolder, "socData.csv"), row.names=FALSE)
+write.csv(paste0(outfolder, "socData_10_20_17.csv"), row.names=FALSE)
